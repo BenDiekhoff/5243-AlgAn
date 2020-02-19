ad81164c3a5509bc213ff94ea5902cf1940226da
BenDiekhoff <ben.diekhoff@gmail.com>
2020-02-19 15:46:36 -0600

    Sorts

--- misc/sorts.py
+++ misc/sorts.py
@@ -1,43 +1,56 @@
+print ("\nBubble Sort\n-----------------------")
 a = [6,3,5,2,1,4,0]
-n = len(a)-1
+print(a)
 
-for i in range(0,n):
+n = len(a)
+print(n)
+for i in range(0,n-1):
 
-    for j in range(0,n-i):
-  
+    for j in range(0,n - 1 - i):
+        print("{} {} {} \n".format(i,j, n-1-i))
+        
         if a[j] > a[j+1]:
             a[j], a[j+1] = a[j+1], a[j] #swap
 
-print(a)
+    print(a)
 
 #insertion sort
+print ("\nInsertion Sort\n-----------------------")
 a = [6,3,5,2,1,4,0]
+print(a)
 
-for i in range(1,len(a)):
-   
+n = len(a)
+
+for i in range(1,n):
     key = a[i]
     j = i -1
     
     while j >=0 and a[j] > key:
-       
         a[j+1] = a[j]
         j -= 1
     
     a[j+1] = key
 
-print(a)
+    print(a)
 
 
 #selection sort
+print ("\nSelection Sort\n-----------------------")
 a = [6,3,5,2,1,4,0]
+print(a)
 
-for i in range(0, len(a)-1):
+n = len(a)
+
+for i in range(0, n-1):
     mindex = i
-    for j in range(i+1, len(a)):
+    
+    for j in range(i+1, n):
         if a[mindex] > a[j]:
             mindex = j
+    
     a[i], a[mindex] = a[mindex], a[i]
-print (a)
+    
+    print (a)
 
 
 
